f5c35335a2d92e41c971a63c34ca90bc8117588f
Farai Matangira <fmatangira05@yahoo.com>
2020-03-27 23:51:00 -0500

    got bcrypt to work for sign up

--- pages/api/signUp.js
+++ pages/api/signUp.js
@@ -1,14 +1,21 @@
+import bcrypt from 'bcrypt';
+const saltRounds = 10;
 import db from '../../models';
 
-export default async function (req,res) {
-    
-    await db.user.create({
-            first_name: req.body.first_name,
-            last_name: req.body.last_name,
-            email: req.body.email,
-            password: req.body.password
-        });
+export default function (req, res) {
+
+    const password = req.body.password;
+
+    bcrypt.hash(password, saltRounds, function (err, hash) {
+
+            db.user.create({
+                first_name: req.body.first_name,
+                last_name: req.body.last_name,
+                email: req.body.email,
+                password: hash
+            });
+    });
 
     res.end();
-    
+
 }
